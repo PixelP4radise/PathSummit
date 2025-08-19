b82b1ba6c1d050140c4394f13433e44e1ec9cf19
Guilherme Proença Araújo <guilhermeproencaaraujo@gmail.com>
2025-08-19 20:22:20 +0100

    changed over form mutex to RwLock

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -1,6 +1,6 @@
 use crate::configuration::Graph;
 use crate::solution::{Solution, SolutionType};
-use std::sync::{mpsc, Arc, Mutex};
+use std::sync::{mpsc, Arc, Mutex, RwLock};
 use std::thread;
 
 pub enum Algorithm {
@@ -8,16 +8,26 @@ pub enum Algorithm {
 }
 
 impl Algorithm {
-    pub fn run(&self, initial_solution: &Solution, edges_cost: Arc<Graph>, iterations: usize) -> Solution {
+    pub fn run(
+        &self,
+        initial_solution: &Solution,
+        edges_cost: Arc<Graph>,
+        iterations: usize,
+    ) -> Solution {
         match self {
             Algorithm::HillClimbing => self.hill_climber(initial_solution, edges_cost, iterations),
         }
     }
 
-    pub fn hill_climber(&self, initial_solution: &Solution, edges_cost: Arc<Graph>, iterations: usize) -> Solution {
+    pub fn hill_climber(
+        &self,
+        initial_solution: &Solution,
+        edges_cost: Arc<Graph>,
+        iterations: usize,
+    ) -> Solution {
         let (tx, rx) = mpsc::channel();
 
-        let best_solution = Arc::new(Mutex::new(initial_solution.clone()));
+        let best_solution = Arc::new(RwLock::new(initial_solution.clone()));
         let iterations = Arc::new(Mutex::new(iterations));
 
         for _ in 0..5 {
@@ -26,34 +36,32 @@ impl Algorithm {
             let thread_tx = tx.clone();
             let edges_cost = edges_cost.clone();
 
-            thread::spawn(move || {
-                loop {
-                    let mut remaining_iterations = iterations.lock().unwrap();
+            thread::spawn(move || loop {
+                let mut remaining_iterations = iterations.lock().unwrap();
 
-                    if *remaining_iterations ==0 {
-                        break;
-                    }
-                    *remaining_iterations -=1;
-
-                    drop(remaining_iterations);
-
-                    let best_solution = best_solution.lock().unwrap();
-
-                    let mut neighbor = best_solution.new_solution(SolutionType::RandomV2);
-
-                    drop(best_solution);
-
-                    neighbor.calculate_cost(&edges_cost);
-
-                    thread_tx.send(neighbor).unwrap();
+                if *remaining_iterations == 0 {
+                    break;
                 }
+                *remaining_iterations -= 1;
+
+                drop(remaining_iterations);
+
+                let best_solution = best_solution.read().unwrap();
+
+                let mut neighbor = best_solution.new_solution(SolutionType::RandomV2);
+
+                drop(best_solution);
+
+                neighbor.calculate_cost(&edges_cost);
+
+                thread_tx.send(neighbor).unwrap();
             });
         }
 
         drop(tx);
 
         for neighbor in rx {
-            let mut best_solution = best_solution.lock().unwrap();
+            let mut best_solution = best_solution.write().unwrap();
 
             if neighbor.is_better_than(&*best_solution) {
                 *best_solution = neighbor;
@@ -61,6 +69,6 @@ impl Algorithm {
             drop(best_solution);
         }
 
-        best_solution.lock().unwrap().clone()
+        best_solution.read().unwrap().clone()
     }
 }
